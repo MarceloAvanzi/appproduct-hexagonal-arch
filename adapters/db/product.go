e77af93754bcef2f8a98a2a8d0f7af7b73c0ab28
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-03-18 20:33:12 -0300

    add db adapter with get method

---
+++ adapters/db/product.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+	"github.com/MarceloAvanzi/appproduct-hexagonal-arch/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func (s *ProductDb) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Preprare("select id, name, price, status from product where id=?")
+	if err != nil {
+		return nil, err
+	}
+	
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nill
+}
